8a428498c41f51f26dd7586e419a6947f2a12cf4
Hieu Le <minhhieu.le@stud.tu-darmstadt.de>
2023-03-29 12:02:23 +0200

    add template for the actual competition

---
+++ main/levels/Level6.java
@@ -0,0 +1,26 @@
+import helperPackage.*;
+public class Level6 {
+
+    public static void main(String[] args) {
+        //Test 1
+        System.out.println();
+        //Test 2
+        System.out.println();
+        //Test 3
+        System.out.println();
+        //Test 4
+        System.out.println();
+        //Test 5
+        System.out.println();
+        //Test 6
+        System.out.println();
+        //Test 7
+        System.out.println();
+        //Test 8
+        System.out.println();
+    }
+
+    public static void mainMethodToSolveTheLevel(){
+
+    }
+}
